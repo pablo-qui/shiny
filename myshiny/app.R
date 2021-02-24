e9854709cecef6e95503b489f044ac3e1280a4eb
pablo-qui <pabloquicios@gmail.com>
2021-02-24 15:38:29 +0100

    addded side panel

--- myshiny/app.R
+++ myshiny/app.R
@@ -20,15 +20,24 @@ ui <- fluidPage(
     # Application title
     titlePanel("This is a new Shiny app"),
     includeMarkdown("references.md"),
-    selectInput("select", label = h3("Plot by type of alimentation"), 
-                choices = list_choices,
-                selected = 1),
-    h1("Plots"),
-    plotOutput(outputId="plot")
+    sidebarLayout(
+        sidebarPanel(
+             selectInput("select", label = h3("Plot by type of alimentation"), 
+        choices = character(0), selected = 1)
+        
+    ),
+    mainPanel(
+        plotOutput(outputId = "plot")
+    )
+)
+
 )
 col_scale <- scale_colour_discrete(limits = list_choices)
 # Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output,session) {
+    updateSelectInput(session,"select",
+                      choices=list_choices,
+                      selected = tail(list_choices,1))
     output$plot <- renderPlot({
         ggplot(msleep%>% filter(vore==input$select), aes(bodywt, sleep_total, colour = vore)) +
             col_scale+
