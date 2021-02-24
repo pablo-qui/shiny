fcd16c6c3115337ff45dd9ee30f90d2ad8c28640
pablo-qui <pabloquicios@gmail.com>
2021-02-24 15:26:01 +0100

    filter

--- myshiny/app.R
+++ myshiny/app.R
@@ -10,19 +10,31 @@
 library(shiny)
 library(tidyverse)
 
+list_choices <-  unique(msleep$vore)
+list_choices <- list_choices[!is.na(list_choices)]
+names(list_choices) <- paste(list_choices,"vore",sep = "")
+
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
     titlePanel("This is a new Shiny app"),
     includeMarkdown("references.md"),
+    selectInput("select", label = h3("Plot by type of alimentation"), 
+                choices = list_choices,
+                selected = 1),
     h1("Plots"),
     plotOutput(outputId="plot")
 )
-data <- runif(20)
+col_scale <- scale_colour_discrete(limits = list_choices)
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-    output$plot <- renderPlot({plot(data)}) 
+    output$plot <- renderPlot({
+        ggplot(msleep%>% filter(vore==input$select), aes(bodywt, sleep_total, colour = vore)) +
+            col_scale+
+            scale_x_log10() +
+            geom_point() 
+    }) 
     
 }
 
