9ef083a99822466e73ee53dc23b6a07b237a2cea
pablo-qui <pabloquicios@gmail.com>
2021-03-03 13:29:55 +0100

    a lot of changes

--- myshiny/app.R
+++ myshiny/app.R
@@ -1,5 +1,5 @@
 #
-# This is a Shiny web application. You can run the application by clicking
+#This is a Shiny web application. You can run the application by clicking
 # the 'Run App' button above.
 #
 # Find out more about building applications with Shiny here:
@@ -7,50 +7,95 @@
 #    http://shiny.rstudio.com/
 #
 
-library(shiny)
-library(tidyverse)
+require(shiny)
+require(tidyverse)
+require("shinyjs")
 
 list_choices <-  unique(msleep$vore)
-list_choices <- list_choices[!is.na(list_choices)]
-names(list_choices) <- paste(list_choices,"vore",sep = "")
+list_choices = list_choices[!is.na(list_choices)]
+names(list_choices) = paste0(list_choices,"vore")
 
 # Define UI for application that draws a histogram
-ui <- navbarPage( "Shiny app",
-    tabPanel("msleep",
-    fluidPage(
+ui <- navbarPage( "ShinyApp",
+                  tabPanel("msleep",
+                           fluidPage(
+                               sidebarLayout(
+                                   sidebarPanel( 
+                                       selectInput("select", label= h3("Plot by type of alimentation"),
+                                                   choices=character(0),
+                                                   selected=1)
+                                   ),
+                                   mainPanel(
+                                       h3("Plots"),
+                                       plotOutput(outputId = "plot")
+                                   )
+                               )
+                               
+                           )),
+                  tabPanel("Random generator",
+                           sidebarLayout(position = "right",
+                                         sidebarPanel(
+                                             selectInput("dist", label = h3("Select the distribution"), 
+                                                         choices = list(Normal="rnorm", Uniform="runif", Exponential="rexp"),
+                                                         selected = 1),
+                                             sliderInput("n_sample", label = h3("Number of samples"), min = 10, 
+                                                         max = 100, value = 50),
+                                             fluidRow(
+                                                 h3(style = "margin-left: 20px; margin-bottom: 0px;", "Number of bins"),
+                                                 column(2,
+                                                        div(style = "margin-top: 37px", checkboxInput("auto_bins", label = "auto", value = TRUE))
+                                                 ),
+                                                 column(10,
+                                                        sliderInput("n_bins", label="", min = 1, max = 50, value = 30)
+                                                 )
+                                             )
+                                         ), # sidebarPanel
+                                         mainPanel(
+                                             plotOutput(outputId = "pulpo")
+                                         ) # mainPanel
+                           ) # sidebarLayout
+                  ), #  tabPanel
+                  tabPanel("References",
+                           p(tags$button(class="btn btn-default", 
+                                         `data-toggle`="collapse", 
+                                         `data-target`="#hola",
+                                         "References")),
+                           div(class="collapse", id="hola",
+                               div(class="card card-body",
+                                   includeMarkdown("references.md")
+                               )),
+                           
+                  ), #  tabPanel
+                  useShinyjs()
+) # navbarPage
 
-    # Application title
-    
-    
-    sidebarLayout(
-        sidebarPanel(
-             selectInput("select", label = h3("Plot by type of alimentation"), 
-        choices = character(0), selected = 1)
-        
-    ),
-    mainPanel(
-        plotOutput(outputId = "plot")
-    )
-)
 
-)
-),
-tabPanel("References",
-         includeMarkdown("references.md")
-)
-)
 col_scale <- scale_colour_discrete(limits = list_choices)
+
 # Define server logic required to draw a histogram
-server <- function(input, output,session) {
-    updateSelectInput(session,"select",
-                      choices=list_choices,
-                      selected = tail(list_choices,1))
-    output$plot <- renderPlot({
-        ggplot(msleep%>% filter(vore==input$select), aes(bodywt, sleep_total, colour = vore)) +
-            col_scale+
+server <- function(input, output, session) {
+    updateSelectInput(session, "select",
+                      choices = list_choices,
+                      selected = tail(list_choices, 1)
+    )
+    
+    output$plot = renderPlot({
+        ggplot(msleep %>% filter(vore==input$select)
+               , aes(bodywt, sleep_total, colour = vore)) +
             scale_x_log10() +
-            geom_point() 
-    }) 
+            col_scale +
+            geom_point()
+    })
+    
+    cmd = reactive(eval(parse(text=paste(input$dist,"(",input$n_sample,")"))));
+    
+    observe(if(input$auto_bins) disable("n_bins") else enable("n_bins") )
+    
+    output$pulpo <- renderPlot(
+        if(input$auto_bins) hist(cmd()) 
+        else hist(cmd(), breaks=input$n_bins)
+    );
+    
     
 }
 
