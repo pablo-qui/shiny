9a2bc6049705afcfb87c47568dec2eb7c6e2588d
pablo-qui <pabloquicios@gmail.com>
2021-03-03 14:37:28 +0100

    report generation

--- myshiny/app.R
+++ myshiny/app.R
@@ -26,20 +26,22 @@ ui <- navbarPage( "ShinyApp",
                                                    selected=1)
                                    ),
                                    mainPanel(
-                                       h3("Plots"),
-                                       plotOutput(outputId = "plot")
+                                       plotOutput(outputId = "plot", click = "plot_click"),
+                                       tableOutput("info")
                                    )
                                )
                                
                            )),
                   tabPanel("Random generator",
                            sidebarLayout(position = "right",
+                                         
                                          sidebarPanel(
                                              selectInput("dist", label = h3("Select the distribution"), 
                                                          choices = list(Normal="rnorm", Uniform="runif", Exponential="rexp"),
                                                          selected = 1),
                                              sliderInput("n_sample", label = h3("Number of samples"), min = 10, 
                                                          max = 100, value = 50),
+                                             actionButton("goButton", "Go!"),
                                              fluidRow(
                                                  h3(style = "margin-left: 20px; margin-bottom: 0px;", "Number of bins"),
                                                  column(2,
@@ -48,10 +50,15 @@ ui <- navbarPage( "ShinyApp",
                                                  column(10,
                                                         sliderInput("n_bins", label="", min = 1, max = 50, value = 30)
                                                  )
-                                             )
+                                             ), # fluidRow
+                                             downloadButton("report", "Generate report")
                                          ), # sidebarPanel
                                          mainPanel(
-                                             plotOutput(outputId = "pulpo")
+                                             tabsetPanel(type = "tabs",
+                                                         tabPanel("Plot", plotOutput("histPlot")),
+                                                         tabPanel("Summary", verbatimTextOutput("histSummary")),
+                                                         tabPanel("Table", tableOutput("histTable"))
+                                             )
                                          ) # mainPanel
                            ) # sidebarLayout
                   ), #  tabPanel
@@ -87,14 +94,55 @@ server <- function(input, output, session) {
             geom_point()
     })
     
-    cmd = reactive(eval(parse(text=paste(input$dist,"(",input$n_sample,")"))));
+        output$info <- renderTable({
+            nearPoints(msleep 
+                       %>% filter(vore == input$select) 
+                       %>% select(name, bodywt,  sleep_total, sleep_rem, sleep_cycle ), 
+                       input$plot_click, threshold = 100, maxpoints = 3,
+                       addDist = TRUE)
+    })
     
-    observe(if(input$auto_bins) disable("n_bins") else enable("n_bins") )
+    samples <- reactive({
+        input$goButton
+        dist <- eval(parse(text=paste(input$dist)))
+        dist(isolate(input$n_sample))
+    })
     
-    output$pulpo <- renderPlot(
-        if(input$auto_bins) hist(cmd()) 
-        else hist(cmd(), breaks=input$n_bins)
-    );
+    observe(if(input$auto_bins) disable("n_bins") else enable("n_bins"))
+    
+    output$histPlot <- renderPlot(
+        hist(samples(), main="Random Generation", 
+             breaks = if(!input$auto_bins) {input$n_bins} else {"Sturges"})
+    )
+    output$histSummary <- renderPrint(summary(samples()))
+    output$histTable <- renderTable(samples())
+    
+    output$report <- downloadHandler(
+        # For PDF output, change this to "report.pdf"
+        filename = "report.html",
+        content = function(file) {
+            # Copy the report file to a temporary directory before processing it, in
+            # case we don't have write permissions to the current working dir (which
+            # can happen when deployed).
+            tempReport <- file.path(tempdir(), "report.Rmd")
+            file.copy("report.Rmd", tempReport, overwrite = TRUE)
+            
+            # Set up parameters to pass to Rmd document
+            params <- list(
+                n_sample = isolate(input$n_sample), 
+                dist = isolate(input$dist), 
+                breaks = if(!isolate(input$auto_bins)) {isolate(input$n_bins)} else {"Sturges"}
+            )
+            
+            # Knit the document, passing in the `params` list, and eval it in a
+            # child of the global environment (this isolates the code in the document
+            # from the code in this app).
+            rmarkdown::render(tempReport, output_file = file,
+                              params = params,
+                              envir = new.env(parent = globalenv())
+            )
+        }
+    )
     
     
 }
